618fa5bebf0e55d0d5092eed1fd732da6b465d5b
hidoldori <hidoldori@gmail.com>
2021-05-11 15:04:54 +0900

    debug

--- src/main/java/com/example/hidoldoriservicea/FeignServiceBInterface.java
+++ src/main/java/com/example/hidoldoriservicea/FeignServiceBInterface.java
@@ -3,7 +3,7 @@ package com.example.hidoldoriservicea;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "kkk", url ="http://127.0.0.1:8082")
+@FeignClient(name = "hidoldori-service-b", url ="")
 public interface FeignServiceBInterface {
 
     @RequestMapping("hidoldori/lab/hidoldori-service-b/")
